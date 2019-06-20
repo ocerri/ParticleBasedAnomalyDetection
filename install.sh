c629aa76857897b909e007933c4aef329bc5d385
Olmo Cerri <olmo.cerri@gmail.com>
2019-06-20 19:38:34 +0200

    First graph net

--- install.sh
+++ install.sh
@@ -6,8 +6,18 @@ fi
 conda create -n PartAN python=3
 conda activate PartAN
 
-conda install h5py numpy
-pip install tensorflow==2.0.0-beta1
+conda install h5py numpy=1.16.4
 conda install jupyter
+
+conda install pytorch torchvision -c pytorch
+# With cuda supports:
+# MacOS Binaries dont support CUDA, install from source if CUDA is needed
+# conda install pytorch torchvision cudatoolkit=10.0 -c pytorch
+
+
 # Not used for the moment
+# pip install tensorflow==2.0.0-beta1
 # conda install -c conda-forge root
+
+# Optional
+# conda install -c conda-forge jupyter_contrib_nbextensions
