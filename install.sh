c08d0c282567f1462e11cecabbf5d034f8db38eb
Olmo Cerri <olmo.cerri@gmail.com>
2019-07-05 17:58:42 +0200

    side

--- install.sh
+++ install.sh
@@ -3,24 +3,18 @@ if ! [ -x "$(command -v conda)" ]; then
   exit 1
 fi
 
-conda create -n PartAN python=3
+conda create -n PartAN python=3.7.3
 conda activate PartAN
 
-conda install h5py numpy=1.16.4
-conda install jupyter
+conda install h5py=2.9.0 numpy=1.16.4 jupyter matplotlib=3.1.0 scikit-learn=0.21.2
+conda install -c conda-forge prettytable jupyter_contrib_nbextensions
+pip install gpustat
 
-conda install pytorch torchvision -c pytorch
+# conda install pytorch=1.1.0 torchvision=0.3.0 -c pytorch
 # With cuda supports:
 # MacOS Binaries dont support CUDA, install from source if CUDA is needed
-# conda install pytorch torchvision cudatoolkit=10.0 -c pytorch
-
-conda install matplotlib
-conda install -c conda-forge prettytable
-pip install gpustat
+conda install pytorch=1.1.0 torchvision=0.3.0 cudatoolkit=10.0 -c pytorch
 
 # Not used for the moment
 # pip install tensorflow==2.0.0-beta1
 # conda install -c conda-forge root
-
-# Optional
-# conda install -c conda-forge jupyter_contrib_nbextensions
